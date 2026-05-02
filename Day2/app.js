9d1daa475fc7ef097e06f5b1dc534d48ab5b3ddf
Akshit1984 <sharmakshit2000@gmail.com>
2026-05-02 16:37:21 +0530

    my first login page

--- Day2/app.js
+++ Day2/app.js
@@ -2,7 +2,7 @@ const login = () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (username === "admin" && password === "password") {
+    if (username === "audi" && password === "280502") {
         alert("Login successful!");
         window.location.href = "app.html";
     } else {
