415b863805f5c802c498a701c6eeba97fe32f9cf
Akshit1984 <sharmakshit2000@gmail.com>
2026-05-05 12:42:18 +0530

    Fetch function

--- Day4/sync.js
+++ Day4/sync.js
@@ -11,13 +11,16 @@
 // }).catch((error) => {
 //     console.error("Error:", error);
 // });
-// setTimeout(() => {
-//     console.log("Data received");
-// }, 5000);
+
+
+
+setTimeout(() => {
+    console.log("Data received");
+}, 5000);
  
-// setInterval(() => {
-//     console.log("Checking for new data...");
-// }, 3000);
+setInterval(() => {
+    console.log("Checking for new data...");
+}, 3000);
  
 
 
@@ -28,11 +31,13 @@
 
 function getData(){
     return new Promise ((resolve, reject) => {
-        setTimeout(() => {
+        if(2>3){
+            setTimeout(() => {
             resolve("Data received");
         }, 2000);
-        (error) => {
-            reject("Error occurred");   
+        }
+        else{
+            reject("Error occurred");
         }
     });
 }
@@ -42,3 +47,23 @@ getData().then((data) => {
 }).catch((error) => {
     console.error("Error:", error);
 });
+
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Akshit " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
+
+
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(res => res.json())
+.then(data => data.filter(d =>d.id % 3 == 0))
+.then(data => data.map(d => (d.title = d.id + " " + d.title)))
+.then(data => console.log(data))
+.catch(error => console.error("Error fetching data:", error));
+
