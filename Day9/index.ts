ee645509097c3165d096d8dfc15009e6944c1907
Akshit1984 <sharmakshit2000@gmail.com>
2026-05-11 12:39:16 +0530

    ts basic

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,7 +1,17 @@
-let name = "Akshit";
-console.log(name);
+// let name = "Akshit";
+// console.log(name);
 
 //for running it  use :: npx tsx index.ts   OR  npx  ts-node index.ts
 
 
-  
+let name: string = "Akshit";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(5, 10));
